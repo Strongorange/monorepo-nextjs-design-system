238da5959d70173f3f5033d2faafe34892db1680
Chanhwi Lee <chanhwi.lee6@gmail.com>
2025-08-13 15:17:00 +0900

    [refactor] 디자인 시스템 구분자 prefix 추가
    - 사용하는 쪽에 설정된 스타일과 디자인 시스템의 충돌 방지

--- packages/design-system/src/components/ui/button.tsx
+++ packages/design-system/src/components/ui/button.tsx
@@ -4,14 +4,13 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "../../lib/utils";
 
-// defualt "bg-primary text-primary-foreground shadow hover:bg-primary/90 ",
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          "bg-reg-50 text-primary-foreground shadow hover:bg-primary/90 ",
+          "bg-primary text-primary-foreground shadow hover:bg-primary/90 ",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
@@ -46,7 +45,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(
+          "ds-Button",
+          buttonVariants({ variant, size, className })
+        )}
         ref={ref}
         {...props}
       />
